ae425946b8a3b4ca3749d2fcbbab9d076590ac89
Ayeza <ayezabashir442@gmail.com>
2023-05-26 23:57:27 +0500

    added lounge component

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Home from './components/Home/Home'
 import Search from './components/Search/Search'
 import Support from './components/Support/Support'
 import Info from './components/Info/Info'
+import Lounge from './components/Lounge/Lounge'
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Search />
       <Support />
       <Info />
+      <Lounge />
     </div>
   )
 }
--- src/components/Lounge/Lounge.jsx
+++ src/components/Lounge/Lounge.jsx
@@ -1,9 +1,51 @@
-import React from 'react'
+import imageGrid from '../../assets/imageGrid.png'
 
 const Lounge = () => {
   return (
-    <div>
-      Lounge
+    <div className="lounge container flex">
+      <div className="sectionContainer grid">
+        <div className="imgDiv">
+          <img src={imageGrid} alt="" />
+        </div>
+
+        <div className="textDiv">
+          <h2>Unaccompanied Minor Lounge</h2>
+          <div className="grids grid">
+
+            <div className="singleGrid">
+              <span className="gridTitle">
+                Help through the airport
+              </span>
+              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Harum consectetur, nesciunt ipsam aliquam eius sint aperiam.</p>
+            </div>
+
+            <div className="singleGrid">
+              <span className="gridTitle">
+                Proority Boarding
+              </span>
+              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Harum consectetur, nesciunt ipsam aliquam eius sint aperiam.</p>
+            </div>
+
+            <div className="singleGrid">
+              <span className="gridTitle">
+                Chauffeur-drive service
+              </span>
+              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Harum consectetur, nesciunt ipsam aliquam eius sint aperiam.</p>
+            </div>
+
+            <div className="singleGrid">
+              <span className="gridTitle">
+                Care on Flight
+              </span>
+              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Harum consectetur, nesciunt ipsam aliquam eius sint aperiam.</p>
+            </div>
+
+          </div>
+        </div>
+
+
+
+      </div>
     </div>
   )
 }
